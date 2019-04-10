2655aa2317e90e58e54ac8a46316287681b2f838
Owen Rubel <orubel@gmail.com>
2019-04-10 16:02:23 -0700

    blocking quartz job from running during tests

--- grails-app/jobs/StatsSchedJob.groovy
+++ grails-app/jobs/StatsSchedJob.groovy
@@ -1,24 +1,26 @@
 import net.nosegrind.apiframework.StatsService
 import net.nosegrind.apiframework.Stat
+import grails.util.Environment
+
 
 class StatsSchedJob {
 
     def statsService
 
     static triggers = {
-        cron name: 'myTrigger2', cronExpression: "0 */1 * ? * *"
+		if (Environment.current != Environment.TEST) {
+			cron name: 'myTrigger2', cronExpression: "0 */10 * ? * *"
+		}
     }
 
-
     def execute() {
-		println "Job Run"
 		def temp = statsService.getStatsCache()
 
 		temp.each(){ it ->
 			//int user = it[0]
 			//int code = it[1]
 			//String uri = it[2]
-			//BigInteger = it[3]
+			//BigInteger timestamp = it[3]
 			Stat st = new Stat(user:it[0],code:it[1],uri:"${it[2]}",timestamp:it[3])
 			if(!st.save(flush:true,failOnError:true)){
 				st.errors.allErrors.each { println(it2) }
