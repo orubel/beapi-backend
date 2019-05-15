2a8dd36fc6f53c46035d553cdf01d4f54219f05a
Owen Rubel <orubel@gmail.com>
2019-05-15 12:07:03 -0700

    saving tag

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -5,6 +5,7 @@
 import net.nosegrind.apiframework.Person
 import net.nosegrind.apiframework.Role
 import net.nosegrind.apiframework.PersonRole
+
 import org.h2.tools.Server
 
 class BootStrap {
@@ -14,56 +15,46 @@ class BootStrap {
     def passwordEncoder
     def grailsApplication
     def springSecurityService
+
 	
     def init = { servletContext ->
         // Throttle
         // only instantiate if this server is 'master'; check config value
         server = Server.createTcpServer(args).start()
 
-        def roles = []
         def networkRoles = grailsApplication.config.apitoolkit.networkRoles
         networkRoles.each { k, v ->
             v.each{ it2 ->
-                if(!roles.contains(it2)){
-                    roles.add(it2)
+                Role role = Role.findByAuthority(it2)
+                if(!role){
+                    role = new Role(authority:it2)
+                    role.save(flush:true,failOnError:true)
                 }
             }
         }
 
-        //def apitoolkit = grailsApplication.config.apitoolkit
-       // apitoolkit.roles.each { it ->
-        roles.each { it ->
-            String currRole = it
-            Role role = Role.findByAuthority(currRole)
-            if(!role){
-                role = new Role(authority:currRole)
-                role.save(flush:true,failOnError:true)
-            }
-        }
 
         Person user = Person.findByUsername("${grailsApplication.config.root.login}")
-
-
-        PersonRole.withTransaction(){ status ->
+        PersonRole.withTransaction() { status ->
             Role adminRole = Role.findByAuthority("ROLE_ADMIN")
 
-            if(!user?.id){
-                user = new Person(username:"${grailsApplication.config.root.login}",password:"${grailsApplication.config.root.password}",email:"${grailsApplication.config.root.email}")
+            if (!user?.id) {
+                user = new Person(username: "${grailsApplication.config.root.login}", password: "${grailsApplication.config.root.password}", email: "${grailsApplication.config.root.email}")
 
-                if(!user.save(flush:true)){
+                if (!user.save(flush: true)) {
                     user.errors.allErrors.each { println it }
                 }
-            }else{
+            } else {
                 // user exists
-                if(!passwordEncoder.isPasswordValid(user.password, grailsApplication.config.root.password, null)){
+                if (!passwordEncoder.isPasswordValid(user.password, grailsApplication.config.root.password, null)) {
                     log.error "Error: Bootstrapped Root Password was changed in config. Please update"
                 }
             }
 
-            if(!user?.authorities?.contains(adminRole)){
-                PersonRole pRole = new PersonRole(user,adminRole)
-                pRole.save(flush:true,failOnError:true)
-            }else{
+            if (!user?.authorities?.contains(adminRole)) {
+                PersonRole pRole = new PersonRole(user, adminRole)
+                pRole.save(flush: true, failOnError: true)
+            } else {
                 // role exists
             }
 
@@ -72,6 +63,6 @@ class BootStrap {
     }
 
     def destroy = {
-	server.stop()
+	    server.stop()
     }
 }
