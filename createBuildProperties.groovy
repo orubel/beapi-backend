db383e6f2ca27398323b2cb6e5ca7224cadbfd78
Owen Rubel <orubel@gmail.com>
2019-06-14 10:14:17 -0700

    fix for build pipeline

--- createBuildProperties.groovy
+++ createBuildProperties.groovy
@@ -5,15 +5,15 @@ String userHome = System.properties['user.home']
 Properties props = new Properties()
 def propsFile = new File("${userHome}/.jenkins/workspace/api-framework/gradle.properties")
 props.load(propsFile.newDataInputStream())
-def appVersion = props.getProperty('apiFrameworkVersion')
-
-println("### VERSION:"+appVersion)
+def buildVersion = props.getProperty('buildVersion')
+def patchVersion = props.getProperty('patchVersion')
+def apiFrameworkVersion = "${buildVersion}.${patchVersion}"
 
 Properties props2 = new Properties()
 def propsFile2 = new File("${userHome}/.jenkins/workspace/beapi-backend/gradle.properties")
 props2.load(propsFile2.newDataInputStream())
 
-props2.setProperty('apiFrameworkVersion', appVersion)
+props2.setProperty('apiFrameworkVersion', apiFrameworkVersion)
 props2.store(propsFile2.newWriter(), null)
 
 
