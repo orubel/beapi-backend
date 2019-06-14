8358ba784514ce2d056aca2797650088458ae0b4
Owen Rubel <orubel@gmail.com>
2019-06-14 10:04:30 -0700

    test

--- createBuildProperties.groovy
+++ createBuildProperties.groovy
@@ -1,36 +1,18 @@
 #!/usr/bin/env groovy
 
-
-
 String userHome = System.properties['user.home']
 
-def appVersion = (System.getenv('BEAPI_BUILD_VERSION'))?System.getenv('BEAPI_BUILD_VERSION'):'1'
-def patch = System.getenv('BUILD_NUMBER')
-def version = "${appVersion}.${patch}"
-
+Properties props = new Properties()
+def propsFile = new File("${userHome}/.jenkins/workspace/api-framework/gradle.properties")
+props.load(propsFile.newDataInputStream())
+def appVersion = props.getProperty('apiFrameworkVersion')
 
 
 Properties props2 = new Properties()
-FileOutputStream out2 = new FileOutputStream("${userHome}/.jenkins/workspace/beapi-backend/gradle.properties")
-FileInputStream in2 = new FileInputStream("${userHome}/.jenkins/workspace/beapi-backend/gradle.properties")
+def propsFile2 = new File("${userHome}/.jenkins/workspace/beapi-backend/gradle.properties")
+props2.load(propsFile2.newDataInputStream())
 
-//props2.load(in2)
-//props2.remove('patchVersion')
-//props2.store(out2, null)
-
-props2.load(in2)
-props2.setProperty('apiFrameworkVersion', version)
-Enumeration<String> enums = (Enumeration<String>) props2.propertyNames()
-println("NAMES:"+enums)
-while (enums.hasMoreElements()) {
-	String key = enums.nextElement()
-	String value = props2.getProperty(key)
-	println("### PRPERTIES:"+key + " : " + value)
-	props2.setProperty("${key}", value)
-}
-props2.store(out2, null)
-out2.close()
-
-
+props2.setProperty('apiFrameworkVersion', appVersion)
+props2.store(propsFile2.newWriter(), null)
 
 
