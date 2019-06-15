3e6f120bae2cd03260bba62e00218e2c77689299
Owen Rubel <orubel@gmail.com>
2019-06-15 10:44:24 -0700

    test

--- createBuildProperties.groovy
+++ createBuildProperties.groovy
@@ -8,15 +8,15 @@ props.load(propsFile.newDataInputStream())
 def buildVersion = props.getProperty('buildVersion')
 def patchVersion = props.getProperty('patchVersion')
 def apiFrameworkVersion = "${buildVersion}.${patchVersion}"
-props.close()
+
 
 Properties props2 = new Properties()
 def propsFile2 = new File("${userHome}/.jenkins/workspace/beapi-backend/gradle.properties")
 props2.load(propsFile2.newDataInputStream())
-//props2.close()
+
 
 props2.setProperty('apiFrameworkVersion', apiFrameworkVersion)
 props2.store(propsFile2.newWriter(), null)
-
+props2.close()
 
 
